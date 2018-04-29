9bf1885f2d5f7ba459ee41d042f1aa1051e7daa5
Thomas Luong <luongt@me.com>
2018-04-29 14:05:35 -0700

    Abstracted the Mock stockQuote json var from Unit tests into its own separate folder+directory

--- Stox/Network/MockHTTPClient.swift
+++
@@ -1,9 +0,0 @@
-//
-//  MockHTTPClient.swift
-//  Stox
-//
-//  Created by Puroof on 4/29/18.
-//  Copyright © 2018 Lithogen. All rights reserved.
-//
-
-import Foundation
--- Stox_Unit_Tests/Stox_Unit_Tests.swift
+++ Stox_Unit_Tests/Stox_Unit_Tests.swift
@@ -14,20 +14,23 @@ import Alamofire
 class Stox_Unit_Tests: XCTestCase {
     var resData: Data? = nil
     
-    let jsonString = """
-            {"symbol":"AAPL","companyName":"Apple Inc.","primaryExchange":"Nasdaq Global Select","sector":"Technology","calculationPrice":"close","open":164.07,"openTime":1524835800489,"close":162.32,"closeTime":1524859200408,"high":164.33,"low":160.63,"latestPrice":162.32,"latestSource":"Close","latestTime":"April 27, 2018","latestUpdate":1524859200408,"latestVolume":32828772,"iexRealtimePrice":162.3,"iexRealtimeSize":100,"iexLastUpdated":1524859195957,"delayedPrice":162.49,"delayedPriceTime":1524862794217,"previousClose":164.22,"change":-1.9,"changePercent":-0.01157,"iexMarketPercent":0.04998,"iexVolume":1640782,"avgTotalVolume":32691754,"iexBidPrice":0,"iexBidSize":0,"iexAskPrice":0,"iexAskSize":0,"marketCap":823613790160,"peRatio":16.68,"week52High":183.5,"week52Low":142.2,"ytdChange":-0.05386146578029324}"
-            """
-    
-    let jsonBody =
-        ["symbol":"AAPL","companyName":"Apple Inc.","primaryExchange":"Nasdaq Global Select","sector":"Technology","calculationPrice":"close","open":164.07,"openTime":1524835800489,"close":162.32,"closeTime":1524859200408,"high":164.33,"low":160.63,"latestPrice":162.32,"latestSource":"Close","latestTime":"April 27, 2018","latestUpdate":1524859200408,"latestVolume":32828772,"iexRealtimePrice":162.3,"iexRealtimeSize":100,"iexLastUpdated":1524859195957,"delayedPrice":162.49,"delayedPriceTime":1524862794217,"previousClose":164.22,"change":-1.9,"changePercent":-0.01157,"iexMarketPercent":0.04998,"iexVolume":1640782,"avgTotalVolume":32691754,"iexBidPrice":0,"iexBidSize":0,"iexAskPrice":0,"iexAskSize":0,"marketCap":823613790160,"peRatio":16.68,"week52High":183.5,"week52Low":142.2,"ytdChange":-0.05386146578029324] as [String : Any]
-
-    
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
         
+        if let path = Bundle.main.path(forResource: "appleStockQuote", ofType: "json") {
+            do {
+                let url = URL(fileURLWithPath: path)
+                let data = try Data(contentsOf: url, options: .alwaysMapped)
+                
+                stub(everything, jsonData(data))
+            } catch let readErr {
+                print("Error reading json: \(readErr)")
+            }
+        }
         
-        stub(everything, json(jsonBody))
+        // Stubbing all requests to return the APPLE JSON fil
+        
     }
     
     override func tearDown() {
@@ -47,26 +50,6 @@ class Stox_Unit_Tests: XCTestCase {
         }
     }
     
-    func testDecodableStockQuoteObject() {
-        
-        let jsonData = Data(self.jsonString.utf8)
-        let jsonDecoder = JSONDecoder()
-        do {
-            let stockQuote = try jsonDecoder.decode(StockQuote.self, from: jsonData)
-            print(stockQuote)
-            XCTAssertEqual(stockQuote.companyName, "Apple Inc.")
-            XCTAssertEqual(stockQuote.symbol, "AAPL")
-            XCTAssertEqual(stockQuote.changePercent, -0.01157)
-            XCTAssertEqual(stockQuote.close, 162.32)
-            XCTAssertEqual(stockQuote.high, 164.33)
-            XCTAssertEqual(stockQuote.low, 160.63)
-            XCTAssertEqual(stockQuote.latestPrice, 162.32)
-            
-        } catch {
-            print(error.localizedDescription)
-        }
-    }
-    
     func testAddingSampleStockQuotes() {
         var stockQuotes = StockQuotes()
         stockQuotes.addSampleStockQuotes()
@@ -74,37 +57,17 @@ class Stox_Unit_Tests: XCTestCase {
         
         XCTAssert(count == 2, "Adding sample stocks to stocksQuotes did not increase count")
     }
-
-    func testMockSession() {
-        let url = "api.exchange.com"
-        let session = MockSession()
-        let client = HTTPClient(session: session)
-        client.get(url: url) {
-            (data, error) in
-            self.resData = data
-        }
-        
-        let pred = NSPredicate(format: "resData != nil")
-        let exp = expectation(for: pred, evaluatedWith: self, handler: nil)
-        let res = XCTWaiter.wait(for: [exp], timeout: 5.0)
-        if res == XCTWaiter.Result.completed {
-            
-            XCTAssertNotNil(resData, "No data recived from the server for InfoView content")
-        } else {
-            XCTAssert(false, "The call to get the URL ran into some other error")
-        }
-    }
     
     func testAlamofireResponse() {
         let url = "https://httpbin.org/get"
         let e = expectation(description: "Alamofire")
         
-        
         Alamofire.request(url).responseJSON { (response) in
             XCTAssertNil(response.error, "Whoops, error \(response.error!.localizedDescription)")
-            
+            XCTAssertEqual(response.response?.statusCode ?? 0, 200, "Status code not 200")
             XCTAssertNotNil(response, "No response")
             
+            // Test Decoded Data
             if let data = response.data {
                 let jsonDecoder = JSONDecoder()
                 do {
@@ -122,17 +85,6 @@ class Stox_Unit_Tests: XCTestCase {
                     print(error.localizedDescription)
                 }
             }
-            
-            XCTAssertNotNil(response.result.value, "No json value")
-            if let json = response.result.value {
-                print("JSON: \(json)")
-                
-            }
-            
-            
-            
-            
-            XCTAssertEqual(response.response?.statusCode ?? 0, 200, "Status code not 200")
             
             e.fulfill()
         }

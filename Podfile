b25247720e6680a123fde7e001c0c796e1b4c718
Thomas Luong <luongt@me.com>
2018-04-30 23:44:51 -0700

    Removed some unused pods

--- Podfile
+++ Podfile
@@ -5,14 +5,16 @@ inhibit_all_warnings! # this will disable all the warnings for all pods
 
 abstract_target 'Stocks' do
     pod 'SnapKit', '~> 4.0.0'
-    pod 'Texture'
     pod 'Alamofire'
-    pod 'Charts'
     pod 'AlamofireImage', '~> 3.3'
-    pod 'Mockingjay'
     
     target 'Stox'
-    target 'Stox_Unit_Tests'
-    target 'StoxUITests'
+    target 'Stox_Unit_Tests' do
+        pod 'Mockingjay'
+    end
+
+    target 'StoxUITests' do
+        pod 'Mockingjay'
+    end
 
 end
--- Pods/Target Support Files/Pods-Stocks-Stox/Pods-Stocks-Stox-frameworks.sh
+++ Pods/Target Support Files/Pods-Stocks-Stox/Pods-Stocks-Stox-frameworks.sh
@@ -145,26 +145,12 @@ strip_invalid_archs() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/AlamofireImage/AlamofireImage.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/Charts/Charts.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/Mockingjay/Mockingjay.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/PINCache/PINCache.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/PINOperation/PINOperation.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/PINRemoteImage/PINRemoteImage.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SnapKit/SnapKit.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/Texture/AsyncDisplayKit.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/URITemplate/URITemplate.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/AlamofireImage/AlamofireImage.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/Charts/Charts.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/Mockingjay/Mockingjay.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/PINCache/PINCache.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/PINOperation/PINOperation.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/PINRemoteImage/PINRemoteImage.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SnapKit/SnapKit.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/Texture/AsyncDisplayKit.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/URITemplate/URITemplate.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
